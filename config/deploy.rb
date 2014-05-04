306e169745c83fc5fe73ec20e10a4c5544382bc5
Joey Crone <joeycrone@gmail.com>
2014-05-04 10:18:37 -0500

    fucked don't stop the stand alone server

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,9 +53,9 @@ namespace :deploy do
 
   before "deploy", "deploy:check_revision"
 
-  before "deploy" do
-      run "cd #{latest_release} && bundle exec rake websocket_rails:stop_server RAILS_ENV=production"
-  end
+#before "deploy" do
+#      run "cd #{latest_release} && bundle exec rake websocket_rails:stop_server RAILS_ENV=production"
+# end
 
   before 'deploy:finalize_update' do
     run "cd #{latest_release} && bundle exec rake websocket_rails:start_server RAILS_ENV=production"
