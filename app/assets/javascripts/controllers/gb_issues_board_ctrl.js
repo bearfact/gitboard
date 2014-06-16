4939824feb5d39b82c1ddc5c50d9495fcf316672
Joey Crone <joey@tragon.com>
2014-06-16 10:41:26 -0500

    more js fixes

--- app/assets/javascripts/controllers/gb_issues_board_ctrl.js
+++ app/assets/javascripts/controllers/gb_issues_board_ctrl.js
@@ -17,7 +17,7 @@ gitBoard.controller("gbIssuesBoardCtrl", function($scope, $routeParams, stateSer
 
     var channel = dispatcher.subscribe_private(stateService.getCurrentOwner()+":"+ stateService.getCurrentRepository()+":"+"issues");
     channel.bind('updated', function(data) {
-      issue = undy.findWhere($scope.issues, {id: data.id});
+      var issue = undy.findWhere($scope.issues, {id: data.id});
       $scope.$apply(function () {
             angular.extend(issue, data);
         });
